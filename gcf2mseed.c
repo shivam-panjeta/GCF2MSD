35539b634fd3e23faf4df869fae32e2911b426a8
Shivam Panjeta <shivam.panjeta@gmail.com>
2016-06-13 12:54:05 +0530

    Updated gcf2mseed programme

--- gcf2mseed.c
+++ gcf2mseed.c
@@ -1,1 +1,43 @@
-.
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+#define MAX_LEN 100
+
+int main(int argc,char *argv[])
+{
+  if(argc==2
+  {
+    char     gcffile[MAX_LEN+1];     /* input GCF file */
+    FILE     *gcf;                   /* pointer to GCF file */
+
+    /* get parameters */
+    strcpy( gcffile, argv[1] );
+
+    gcf = fopen( gcffile, "r" );
+    if  (gcf == NULL)  {
+  		fprintf(stderr, "%s: cannot open input file %s\n", argv[0], gcffile );
+  		return 1;
+  	} /*endif*/
+
+    system(dirf);
+  /* Run dirf command */
+
+    system(gursei);
+  /* Run gursei command */       /* converts gcf to seisan */
+
+    system(wavetool);
+  /* Run wavetool command */     /* converts seisan to mseed */
+
+
+    fclose( gcf );
+    return 0;
+  }
+
+  else
+  {
+    printf("Wrong input given\n" );
+    return 1;
+  }
+}
