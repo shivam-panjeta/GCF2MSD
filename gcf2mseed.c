d2bba8ac202e8832992e41260f02d78c970d247b
Shivam Panjeta <shivam.panjeta@gmail.com>
2016-06-14 11:26:51 +0530

    Modified gcf2mseed program

--- gcf2mseed.c
+++ gcf2mseed.c
@@ -2,42 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
 
-
-#define MAX_LEN 100
-
-int main(int argc,char *argv[])
+int main(void)
 {
-  if(argc==2
+  int year,month,date,n,i;
+str[100];
+  printf("Tell the year,month and date of records to be considered.\n");
+  scanf("%d%d%d",&year,&month,&date);
+  printf("How many records you want to work with?\n");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
   {
-    char     gcffile[MAX_LEN+1];     /* input GCF file */
-    FILE     *gcf;                   /* pointer to GCF file */
-
-    /* get parameters */
-    strcpy( gcffile, argv[1] );
-
-    gcf = fopen( gcffile, "r" );
-    if  (gcf == NULL)  {
-  		fprintf(stderr, "%s: cannot open input file %s\n", argv[0], gcffile );
-  		return 1;
-  	} /*endif*/
-
-    system(dirf);
-  /* Run dirf command */
-
-    system(gursei);
-  /* Run gursei command */       /* converts gcf to seisan */
-
-    system(wavetool);
-  /* Run wavetool command */     /* converts seisan to mseed */
-
-
-    fclose( gcf );
-    return 0;
+    sprintf(str,"dirf %d%d%d_%02d*",year,month,date,i);
+    system(str);
+    system("./gursei.m");
   }
-
-  else
-  {
-    printf("Wrong input given\n" );
-    return 1;
-  }
+  return 0;
 }
