5849b4e1d6d404a31a66c7820a5e0ddd780bd7fa
Shivam Panjeta <shivam.panjeta@gmail.com>
2016-06-17 22:17:09 +0530

    Implemented wavetool command & combined mSEED files to make one day file

--- gcf2mseed.c
+++ gcf2mseed.c
@@ -5,16 +5,42 @@
 int main(void)
 {
   int year,month,date,n,i;
-str[100];
+  char gur[100],wav[100],final_file[100],sys_file_1[100],sys_file_2[100];
   printf("Tell the year,month and date of records to be considered.\n");
   scanf("%d%d%d",&year,&month,&date);
-  printf("How many records you want to work with?\n");
-  scanf("%d",&n);
-  for(i=0;i<n;i++)
+
+  for(i=0;i<24;i++)
   {
-    sprintf(str,"dirf %d%d%d_%02d*",year,month,date,i);
+    sprintf(gur,"dirf %d%d%d_%02d*",year,month,date,i);
     system(str);
     system("./gursei.m");
+  }
+  for(i=0;i<24;i++)
+  {
+    sprintf(wav,"dirf %d-%d-%d-%02d*",year,month,date,i);
+    system(wav);
+    system("./mwavetool.sh");
+  }
+
+  sprintf(final_file,"%d-%d-%d-0000-M.DG64_001_BH_Z",year,month,date);
+  for(i=0;i<12;i++)
+  {
+    sprintf(sys_file_1,"%d-%d-%d-%02d00-00M.DG64_001_BH_Z",year,month,date,2*i);
+    sprintf(sys_file_2,"%d-%d-%d-%02d00-00M.DG64_001_BH_Z",year,month,date,2*i+1);
+    FILE *fp1 = fopen(sys_file_1, "r");
+    FILE *fp2 = fopen(sys_file_2, "r")
+    FILE *fp  = fopen(final_file, "w");
+    char c;
+
+    while ((c = fgetc(fp1)) != EOF)
+      fputc(c, fp);
+
+    while ((c = fgetc(fp2)) != EOF)
+      fputc(c, fp);
+
+    fclose(fp1);
+    fclose(fp2);
+    fclose(fp);
   }
   return 0;
 }
